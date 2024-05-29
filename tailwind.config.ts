ebbafb960f95307493312f6aef2e200f617dd77d
Toby Liu <ybot1122@gmail.com>
2024-05-29 14:00:41 -0700

    Removed nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { nextui } from "@nextui-org/theme";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -6,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/[object Object].js",
   ],
   theme: {
     animation: {
@@ -19,6 +17,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [],
 };
 export default config;
