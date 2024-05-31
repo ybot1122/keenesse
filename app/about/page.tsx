8ce58a879b2b4f57aa0e74e4e3dc8ea65142d063
Toby Liu <ybot1122@gmail.com>
2024-05-30 20:17:54 -0700

    More about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,32 @@ export const metadata: Metadata = {
   title: "About - Keenesse Coaching & Consulting",
 };
 
+const testimonials = [
+  {
+    message:
+      "Since speaking with Daisy, I have gained confidence to network on LinkedIn",
+  },
+  {
+    message:
+      "Thank you so much, Daisy! You gave me really PRACTICAL things I could change!",
+    from: "Linda",
+  },
+  {
+    message:
+      "I appreciate Daisy's honest feedback and willingess to help with career advice.",
+  },
+  {
+    message:
+      "The exercise to identify gap analysis was the most helpful from our session.",
+    from: "Jessi",
+  },
+];
+
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="bg-gray w-full p-10 md:p-24 flex justify-center flex-wrap md:flex-row-reverse">
-        <div className="w-full md:w-1/2 max-w-[420px] h-[50vw] max-h-[420px]">
+        <div className="w-full md:w-1/2 max-w-[620px] h-[50vw] max-h-[420px]">
           <div className="relative w-full h-full">
             <Image
               src="/my-pic.jpeg"
@@ -20,33 +41,46 @@ export default function About() {
             />
           </div>
         </div>
-        <div className="bg-white p-10 text-blue w-full md:w-1/2  max-w-[420px]">
-          <h3 className="text-3xl mb-3">Hi, I&apos;m Daisy</h3>
-          <p className="text-xl mb-3">
+        <div className="bg-white p-10 text-blue w-full md:w-1/2 lg:p-20 max-w-[620px]">
+          <h3 className="text-4xl mb-5">Hi, I&apos;m Daisy</h3>
+          <p className="text-xl mb-3 leading-8">
             I started Keenesse with one goal in mind: to connect people with a
             partner who has been in their shoes and can help them navigate their
             work, studies and life with finesse. I strongly believe in the power
             of coaching, accountability and support.
           </p>
-          <p className="text-xl mb-3">
+          <p className="text-xl mb-3 leading-8">
             I have a 12+ years track record of growing individuals and building
             projects from the ground up in business, non-profit and technology
             sectors amongst others. I provide consulting services on a wide
             array of areas. To name a few: software engineering, product/program
             management, education, research and development.
           </p>
-          <p className="text-xl mb-3">
+          <p className="text-xl mb-3 leading-8">
             I truly care about each client. I take the time to understand their
             needs and I have dedicated the last 4 years to making myself
             available to my clients around the globe.
           </p>
-          <p className="text-xl mb-3">
+          <p className="text-xl mb-3 leading-8">
             Still interested?{" "}
             <Link href="/appointments" className="underline">
               Let&apos;s work together!
             </Link>
           </p>
         </div>
+      </div>
+
+      <div className="flex bg-blue w-full p-5 items-center flex-wrap justify-center">
+        <h3 className="text-4xl text-white text-center my-10 w-full">
+          What Clients are Saying
+        </h3>
+
+        {testimonials.map((t) => (
+          <div className="bg-white m-5 text-blue text-center p-10 max-w-[520px]">
+            <p className="text-2xl">&ldquo;{t.message}&rdquo;</p>
+            {t.from ? <p className="mt-5">{t.from}</p> : null}
+          </div>
+        ))}
       </div>
       <h1>about</h1>
     </main>
