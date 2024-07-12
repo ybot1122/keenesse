439eed0ba9b55aba3223e7c5500f094ad8d3c411
Toby Liu <ybot1122@gmail.com>
2024-07-11 17:36:39 -0700

    Stripe webhook

---
+++ app/stripe_webhook/route.ts
@@ -0,0 +1,58 @@
+import stripe from "stripe";
+import { NextResponse, type NextRequest } from "next/server";
+
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET ?? "";
+const stripeApiSecret = process.env.STRIPE_API_KEY ?? "";
+
+export async function POST(request: NextRequest) {
+  const stripe_api = new stripe(stripeApiSecret);
+
+  let event: any = request.body;
+
+  const raw_body_str = await request.text();
+
+  if (!event) {
+    return new NextResponse("Empty body", {
+      status: 400,
+    });
+  }
+
+  // Only verify the event if you have an endpoint secret defined.
+  // Otherwise use the basic event deserialized with JSON.parse
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = request.headers.get("stripe-signature") ?? "";
+    try {
+      event = stripe_api.webhooks.constructEvent(
+        raw_body_str,
+        signature,
+        endpointSecret,
+      );
+    } catch (err: any) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return new NextResponse("Webhook signature verification failed", {
+        status: 400,
+      });
+    }
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case "payment_method.attached":
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  return new NextResponse("hi");
+}
