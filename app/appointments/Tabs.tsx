addc4a3ea625a3c06c4f222f3abd4d4fa1f603c9
Toby Liu <ybot1122@gmail.com>
2024-08-03 20:36:49 -0700

    New offerings

--- app/appointments/Tabs.tsx
+++ app/appointments/Tabs.tsx
@@ -6,23 +6,23 @@ export default function Tabs({}) {
     <div className="bg-blue pb-20 px-10">
       <Inquiry />
       <Packages />
-      <AdHoc />
     </div>
   );
 }
 
 const Inquiry = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 text-center gap-2">
-      <div className="col-span-1 md:col-span-2">
+    <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-2 mb-20">
+      <div className="col-span-1 md:col-span-3">
         <h1 className="text-white text-4xl pb-5 pt-10" id="inquiry">
-          Inquiry
+          Single Sessions
         </h1>
       </div>
       <BuyCard
         title="Free Consultation"
         href="https://calendly.com/keenesse/free30min"
-        indented={false}
+        indented={true}
+        price={"FREE"}
         bg="white"
         details={[
           "30 minutes",
@@ -30,83 +30,6 @@ const Inquiry = () => {
           "Video Conferencing.",
         ]}
       />
-      <BuyCard
-        details={[
-          "45 minutes",
-          "Initial Inquiry for business proposals or scheduling meetings for billing via invoice under contract.",
-          "Video Conferencing.",
-        ]}
-        title="Business Consultation"
-        href="https://calendly.com/keenesse/30min"
-        indented={false}
-        bg="white"
-      />
-    </div>
-  );
-};
-
-const Packages = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 text-center mt-10 gap-2">
-      <div className="col-span-1 md:col-span-3">
-        <h1
-          className="text-white text-4xl pb-5 pt-10 border-t border-gray"
-          id="packages"
-        >
-          Packages
-        </h1>
-      </div>
-      <BuyCard
-        title="TODO"
-        href="https://calendly.com/keenesse/free30min"
-        indented={true}
-        bg="white"
-        details={[
-          "Video conferencing weekly for 30 mins",
-          "Daily access: coaching and check-ins via email",
-          "Automated reminders and billing; cancel anytime",
-          "Sessions don't expire, unlimited rescheduling",
-        ]}
-      />
-      <BuyCard
-        details={[
-          "12 Video conferencing per year for 60 minutes (or 2x 30 mins)",
-          "Daily access: coaching and check-ins via email",
-          "Automated reminders and billing; cancel anytime",
-          "Sessions don't expire, unlimited rescheduling",
-        ]}
-        title="TODO"
-        href="https://calendly.com/keenesse/30min"
-        indented={false}
-        bg="white"
-      />
-      <BuyCard
-        details={[
-          "4 Video conferencing per year for 60 minutes",
-          "Daily access: coaching and check-ins via email",
-          "Automated reminders and billing; cancel anytime",
-          "Sessions don't expire, unlimited rescheduling",
-        ]}
-        title="TODO"
-        href="https://calendly.com/keenesse/30min"
-        indented={true}
-        bg="white"
-      />
-    </div>
-  );
-};
-
-const AdHoc = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 text-center mt-10 gap-2">
-      <div className="col-span-1 md:col-span-2">
-        <h1
-          className="text-white text-4xl pb-5 pt-10 mt-10 border-t border-gray"
-          id="adhoc"
-        >
-          Ad-Hoc Sessions
-        </h1>
-      </div>
       <BuyCard
         title="Single Session - 30mins"
         href="https://calendly.com/keenesse/30min"
@@ -128,7 +51,58 @@ const AdHoc = () => {
           "Video Conferencing.",
         ]}
         href="https://calendly.com/keenesse/60min"
+        indented={true}
+        bg="white"
+      />
+    </div>
+  );
+};
+
+const Packages = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 text-center mt-10 gap-2">
+      <div className="col-span-1 md:col-span-3">
+        <h1
+          className="text-white text-4xl pb-5 pt-10 border-t border-gray"
+          id="packages"
+        >
+          Packages
+        </h1>
+      </div>
+      <BuyCard
+        title="60 Min Session x4"
+        href="https://calendly.com/keenesse/free30min"
+        indented={true}
+        bg="white"
+        details={[
+          "Video conferencing weekly for 30 mins",
+          "Daily access: coaching and check-ins via email",
+          "Automated reminders and billing; cancel anytime",
+          "Sessions don't expire, unlimited rescheduling",
+        ]}
+      />
+      <BuyCard
+        details={[
+          "12 Video conferencing per year for 60 minutes (or 2x 30 mins)",
+          "Daily access: coaching and check-ins via email",
+          "Automated reminders and billing; cancel anytime",
+          "Sessions don't expire, unlimited rescheduling",
+        ]}
+        title="30 Min Session x12"
+        href="https://calendly.com/keenesse/30min"
         indented={false}
+        bg="white"
+      />
+      <BuyCard
+        details={[
+          "4 Video conferencing per year for 60 minutes",
+          "Daily access: coaching and check-ins via email",
+          "Automated reminders and billing; cancel anytime",
+          "Sessions don't expire, unlimited rescheduling",
+        ]}
+        title="TODO"
+        href="https://calendly.com/keenesse/30min"
+        indented={true}
         bg="white"
       />
     </div>
--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -21,22 +21,16 @@ export default function Appointments() {
           </p>
           <div className="text-center mt-5">
             <Link
-              className="rounded-full border-black border p-2 m-2 bg-white inline-block hover:motion-safe:scale-125 transition-transform duration-200 ease-in-out "
+              className="rounded-full border-black border p-2 m-5 bg-white inline-block hover:motion-safe:scale-125 transition-transform duration-200 ease-in-out "
               href="#inquiry"
             >
-              Inquiry
+              Single Sessions
             </Link>
             <Link
-              className="rounded-full border-black border p-2 m-2 bg-white inline-block hover:motion-safe:scale-125 transition-transform duration-200 ease-in-out "
+              className="rounded-full border-black border p-2 m-5 bg-white inline-block hover:motion-safe:scale-125 transition-transform duration-200 ease-in-out "
               href="#packages"
             >
               Packages
-            </Link>
-            <Link
-              className="rounded-full border-black border p-2 m-2 bg-white inline-block hover:motion-safe:scale-125 transition-transform duration-200 ease-in-out "
-              href="#adhoc"
-            >
-              Ad-Hoc Sessions
             </Link>
           </div>
         </div>
