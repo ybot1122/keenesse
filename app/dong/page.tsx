9306af1e895595b0a8caef60b0b70de2f139c397
Toby Liu <ybot1122@gmail.com>
2024-09-28 13:58:10 -0700

    fix build

--- app/dong/page.tsx
+++ app/dong/page.tsx
@@ -7,7 +7,9 @@ export default function Appointments() {
     <main className="min-h-screen">
       <div className="bg-gray w-full p-10 md:pt-10 md:pb-10 flex justify-center flex-wrap md:flex-row-reverse">
         <div className="w-full max-w-[1024px]">
-          <h1 className="text-4xl text-center block text-blue">Dong's Page</h1>
+          <h1 className="text-4xl text-center block text-blue">
+            Dong&apos;s Page
+          </h1>
           <p className="text-center max-w-[725px] text-lg my-5 mx-auto">
             Description
           </p>
